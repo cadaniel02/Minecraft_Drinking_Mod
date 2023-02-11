e73ba19682f2f0ea89c27ab4e33add25f4e4561d
Zach Russell <zachrussell111@gmail.com>
2023-02-11 13:03:01 -0600

    Death Text and Finish drinks

--- src/main/java/mod/drinking/my/client/DrinkHUD.java
+++ src/main/java/mod/drinking/my/client/DrinkHUD.java
@@ -21,7 +21,8 @@ import net.minecraft.client.gui.GuiComponent;
 
 public class DrinkHUD {
 
-    public static float deathOpacity = 0f;
+    public static int drinkThisMany;
+    public static boolean drinking = false;
     public static float opacity = 1.0f;
     private static final ResourceLocation FILLED_THIRST = new ResourceLocation(DrinkingMod.MODID,
             "textures/drinks/filled_thirst.png");
@@ -35,6 +36,11 @@ public class DrinkHUD {
 
     private static final ResourceLocation LIFETIME = new ResourceLocation(DrinkingMod.MODID,
             "textures/drinks/total.png");
+    private static final ResourceLocation TAKE = new ResourceLocation(DrinkingMod.MODID,
+            "textures/drinks/take.png");
+
+    private static final ResourceLocation SIPS = new ResourceLocation(DrinkingMod.MODID,
+            "textures/drinks/sips.png");
     private static final ResourceLocation EMPTY_THIRST = new ResourceLocation(DrinkingMod.MODID,
             "textures/drinks/empty_thirst.png");
 
@@ -44,17 +50,46 @@ public class DrinkHUD {
         int y = height;
 
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
+
+
         sipTriggerText(poseStack, width);
 
 
-        drawTotalSipAmount(poseStack, width, height);
-        drawLifetimeText(poseStack, width, height);
+        int totalX = width - width/5 + 11;
+        int totalY = height/4 + 12;
+        int size = 7;
+        int number = ClientSipData.getTotalSips();
 
-        drawSipBox(poseStack, width, height);
-        drawCurSipAmount(poseStack, width, height);
+
+        if(drinking) {
+            totalX = width - width/2 - 25;
+            totalY = 84;
+            size = 52;
+            number = drinkThisMany;
+            drawTotalSipAmount(poseStack, size, totalX, totalY, number);
+            drawTakeText(poseStack, width, height);
+        }
+        else {
+            drawTotalSipAmount(poseStack, size, totalX, totalY, number);
+            drawLifetimeText(poseStack, width, height);
+
+            drawSipBox(poseStack, width, height);
+            drawCurSipAmount(poseStack, width, height);
+        }
+
 
         });
 
+    private static void drawTakeText(PoseStack poseStack, int width, int height){
+        int promptSize = 200;
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, TAKE);
+        GuiComponent.blit(poseStack,width/4 - 100 , 10,0,0,promptSize,promptSize,
+                promptSize,promptSize);
+        RenderSystem.setShaderTexture(0, SIPS);
+        GuiComponent.blit(poseStack,width/4 + 100 , 10,0,0,promptSize,promptSize,
+                promptSize,promptSize);
+    }
 
     private static void drawLifetimeText(PoseStack poseStack, int width, int height) {
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
@@ -62,21 +97,21 @@ public class DrinkHUD {
         GuiComponent.blit(poseStack,width - width/5 - 8 , height/7 + 16,0,0,80,60,
                 80,60);
     }
-    private static void drawTotalSipAmount(PoseStack poseStack, int width, int height){
+    private static void drawTotalSipAmount(PoseStack poseStack, int size, int width, int height, int totalSips){
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        int totalSips = ClientSipData.getTotalSips();
+
         if(totalSips< 10){
-            int textSize = 7;
+            int textSize = size;
             //files has number one higher than glyph
             int numName = totalSips+1;
             ResourceLocation number = new ResourceLocation(DrinkingMod.MODID,
                     "textures/nums/whitenums" + numName +  ".png");
             RenderSystem.setShaderTexture(0, number);
-            GuiComponent.blit(poseStack,width - width/5 + 11 , height/4 + 12,0,0,textSize,textSize,
+            GuiComponent.blit(poseStack,width , height,0,0,textSize,textSize,
                     textSize,textSize);
         }
         else if(totalSips < 100){
-            int textSize = 7;
+            int textSize = size;
             //files has number one higher than glyph
 
             int numName1 = (totalSips / 10) + 1;
@@ -86,18 +121,18 @@ public class DrinkHUD {
             ResourceLocation number = new ResourceLocation(DrinkingMod.MODID,
                     "textures/nums/whitenums" + numName1 +  ".png");
             RenderSystem.setShaderTexture(0, number);
-            GuiComponent.blit(poseStack,width - width/5 + 7 , height/4 + 12,0,0,textSize,textSize,
+            GuiComponent.blit(poseStack,width - 4*size/7 , height,0,0,textSize,textSize,
                     textSize,textSize);
 
             //digit 2 render
             ResourceLocation number2 = new ResourceLocation(DrinkingMod.MODID,
                     "textures/nums/whitenums" + numName2 +  ".png");
             RenderSystem.setShaderTexture(0, number2);
-            GuiComponent.blit(poseStack,width - width/5 + 11 , height/4 + 12,0,0,textSize,textSize,
+            GuiComponent.blit(poseStack,width , height,0,0,textSize,textSize,
                     textSize,textSize);
         }
         else{
-            int textSize = 7;
+            int textSize = size;
             //files has number one higher than glyph
             int numName0 = (totalSips / 100) + 1;
             int numName1 = ((totalSips - (100 *(totalSips/100))) / 10) + 1;
@@ -108,21 +143,21 @@ public class DrinkHUD {
             ResourceLocation number = new ResourceLocation(DrinkingMod.MODID,
                     "textures/nums/whitenums" + numName0 +  ".png");
             RenderSystem.setShaderTexture(0, number);
-            GuiComponent.blit(poseStack,width - width/5 + 4 , height/4 + 12,0,0,textSize,textSize,
+            GuiComponent.blit(poseStack,width + 7 , height,0,0,textSize,textSize,
                     textSize,textSize);
 
             //digit 1 render
             ResourceLocation number1 = new ResourceLocation(DrinkingMod.MODID,
                     "textures/nums/whitenums" + numName1 +  ".png");
             RenderSystem.setShaderTexture(0, number1);
-            GuiComponent.blit(poseStack,width - width/5 + 9 , height/4 + 12,0,0,textSize,textSize,
+            GuiComponent.blit(poseStack,width + 2 , height,0,0,textSize,textSize,
                     textSize,textSize);
 
             //digit 2 render
             ResourceLocation number2 = new ResourceLocation(DrinkingMod.MODID,
                     "textures/nums/whitenums" + numName2 +  ".png");
             RenderSystem.setShaderTexture(0, number2);
-            GuiComponent.blit(poseStack,width - width/5 + 14 , height/4 + 12,0,0,textSize,textSize,
+            GuiComponent.blit(poseStack,width + 7 , height,0,0,textSize,textSize,
                     textSize,textSize);
 
 
