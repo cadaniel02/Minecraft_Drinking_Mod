2a37e750811fc02ec79c99d51fac1fc04aac4eb6
pastor-of-muppets <48391446+pastor-of-muppets@users.noreply.github.com>
2023-02-11 19:23:55 -0600

    removed comments xd

--- src/main/java/mod/drinking/my/events/ModEvents.java
+++ src/main/java/mod/drinking/my/events/ModEvents.java
@@ -71,16 +71,6 @@ public class ModEvents {
         });
     }
 
-//    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-//        if(event.side == LogicalSide.SERVER) {
-//            event.player.getCapability(PlayerThirstProvider.PLAYER_THIRST).ifPresent(thirst -> {
-//                if(thirst.getThirst() > 0 && event.player.getRandom().nextFloat() < 0.005f) { // Once Every 10 Seconds on Avg
-//                    thirst.subThirst(1);
-//                    ModMessages.sendToPlayer(new ThirstDataSyncS2CPacket(thirst.getThirst()), ((ServerPlayer) event.player));
-//                }
-//            });
-//        }
-
     @SubscribeEvent
     public static void onEnterWaterAddSip(TickEvent.PlayerTickEvent event){
         if(event.side == LogicalSide.SERVER && event.phase.toString().equals("START")) {
